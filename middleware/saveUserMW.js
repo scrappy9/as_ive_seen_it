dc17b75d7b488a7d3e0dc9fa9f3245ad3b9770d6
scrappy9 <ferko1276@gmail.com>
2020-07-12 09:47:37 +0200

    alerts
    Colorful alerts on login page.

--- middleware/saveUserMW.js
+++ middleware/saveUserMW.js
@@ -3,38 +3,21 @@ const bcrypt = require('bcrypt');
 
 module.exports = function() {
     return function (req, res, next){
-        /*
-        let user = new user_model();
-        user.username = req.body.username;
-        user.reg_date = Date.now();
-        user.email = req.body.email;
-
-        //user.password = req.body.password; //TODO hash
-        console.log(user.password);
-        user.save();
-        */
-
         bcrypt.genSalt(10)
         .then(salt => {
-            console.log(`Salt: ${salt}`);
             return bcrypt.hash(req.body.password, salt);
           })
-          .then(hash => {
-            console.log(`Hash: ${hash}`);
+        .then(hash => {
+          
+          let user = new user_model();
+          user.username = req.body.username;
+          user.reg_date = Date.now();
+          user.email = req.body.email;    
+          user.password = hash;
+          user.save();
 
-            // Store hash in your password DB.
-            let user = new user_model();
-            user.username = req.body.username;
-            user.reg_date = Date.now();
-            user.email = req.body.email;    
-            user.password = hash;
-            console.log(user.password);
-            user.save();
-
-            return next();
-          })
-          .catch(err => res.redirect("/login?error=internal_error"));
-
-          //return next();          
+          return next();
+        })
+        .catch(err => res.redirect("/login?error=db_error"));        
     };
 }
