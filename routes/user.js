c351d32eac9f0b83d3425f4b403c2844bc17e1cf
scrappy9 <ferko1276@gmail.com>
2020-07-13 07:51:13 +0200

    username fix

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,14 @@ const express = require("express");
 const authMW = require("../middleware/authMW");
 const renderMW = require("../middleware/renderMW");
 const getCommentsByUserMW = require("../middleware/getCommentsByUser");
+const getUserById = require("../middleware/getUserById");
 
 const router = express.Router();
 
 router.get("/:user_id",
     authMW(),
     getCommentsByUserMW(),
+    getUserById(),
     renderMW("../views/pages/user.ejs")
 );
 
